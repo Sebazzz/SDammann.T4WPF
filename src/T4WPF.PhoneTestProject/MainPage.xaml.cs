bd270a9cf66b3f2a92dead3f4c5c740d7481da49
Sebazzz <triadsebas@gmail.com>
2012-06-30 16:05:25 +0200

    Added examples

--- src/T4WPF.PhoneTestProject/MainPage.xaml.cs
+++ src/T4WPF.PhoneTestProject/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿namespace T4WPF.PhoneTestProject {
+    using System.Collections.Generic;
     using System.Windows.Navigation;
     using Microsoft.Phone.Controls;
 
@@ -13,6 +14,27 @@
             if (e.NavigationMode == NavigationMode.New) {
                 this.ContentPanel.Child = TestHelper.ConvertToGrid(this.NavigationContext.QueryString);
             }
+        }
+
+        private void anObjAppBarMenuItem_Click(object sender, System.EventArgs e) {
+            this.NavigationService.Navigate(NavigationDestination.Pages.Auth.LogonPage.WithQuery(new {useFacebook=true,testFlag=1337, quickAndEasy="very true"}));
+        }
+
+        private void paramsAppBarMenuItem_Click(object sender, System.EventArgs e)
+        {
+            this.NavigationService.Navigate(NavigationDestination.Pages.AboutPage.WithQuery("showEpicMessage", "true",
+                                                                                            "showTrialMessage", "false"));
+        }
+
+        private void queryAppBarMenuItem_Click(object sender, System.EventArgs e) {
+            this.NavigationService.Navigate(NavigationDestination.MainPage.WithQuery("hasNavigatedBack=true&isExample=true"));
+        }
+
+        private void dictAppBarMenuItem_Click(object sender, System.EventArgs e) {
+            this.NavigationService.Navigate(NavigationDestination.Pages.AboutPage.WithQuery(new Dictionary<string, object> {
+                                                                                                                                   {"isUsingDictionary", true},
+                                                                                                                                   {"verboseSyntax", "very true"}
+                                                                                                                           }));
         }
     }
 }
---
+++ src/T4WPF.PhoneTestProject/Pages/Auth/LogonPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace T4WPF.PhoneTestProject.Pages.Auth {
+    using System.Windows.Navigation;
+
+
+    public partial class LogonPage : PhoneApplicationPage {
+        public LogonPage() {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            if (e.NavigationMode == NavigationMode.New) {
+                this.ContentPanel.Child = TestHelper.ConvertToGrid(this.NavigationContext.QueryString);
+            }
+        }
+    }
+}
